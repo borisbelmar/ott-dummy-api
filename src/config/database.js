9be216d65993d91ba8a6bb121917137981e9875a
Boris Belmar <borisbelmarm@gmail.com>
2020-01-22 15:54:47 -0300

    Testing

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+console.log(process.env.MONGODB_CONN_STR);
 
 mongoose.connect(process.env.MONGODB_CONN_STR, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
-        useUnifiedTopology: true
+        /* useUnifiedTopology: true */
     })
     .then(res => console.log('DB is connected'))
     .catch(err => console.error(err));
